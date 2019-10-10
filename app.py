d9b602c38357b01690c347e891f2e9f94de263ee
Jack Morris <jxmorris12@gmail.com>
2019-10-10 15:29:39 -0400

    sqlite3 and better queries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, g
 from paper import Paper
 from flask import redirect, render_template, request
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def index():
     """ Display all papers. """
     papers = Paper.get_all()
+    print('papers:', papers)
     return render_template('index.html', papers=papers)
 
 @app.route('/add')
@@ -15,12 +16,25 @@ def add():
     """ Add a new paper. """
     return render_template('add.html')
 
+@app.route('/paper/<id>')
+def view_paper(id):
+    """ Add a new paper. """
+    paper = Paper.get_paper_by_id(id)
+    return render_template('paper.html', paper=paper)
+
 @app.route('/add_paper', methods=['POST'])
 def add_paper():
     params = request.form
     paper = Paper.from_params(params)
     paper.save()
     return redirect('/') # @TODO highlight paper in list after redirect.
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- paper.py
+++ paper.py
@@ -1,30 +1,77 @@
 import config
 import dateutil.parser
+import json
+import sqlite3
 
-from tinydb import TinyDB, where
+from flask import g # global flask variables
+from tinydb import TinyDB, where, Query
 
-db = TinyDB(config.db_path)
+DATABASE = './database.db'
+
+# Helper function for getting the database.
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+# Helper function for creating new table.
+def create_paper_table():
+    db = get_db()
+    create_table_sql = '''CREATE TABLE IF NOT EXISTS papers (
+        id integer PRIMARY KEY,
+        title text NOT NULL,
+        source text NOT NULL,
+        datePublished text NOT NULL,
+        dateRead text NOT NULL,
+        summary text NOT NULL,
+        futureWork text NOT NULL,
+        otherThoughts text NOT NULL
+    );'''
+    try:
+        c = db.cursor()
+        c.execute(create_table_sql)
+        db.commit()
+    except Error as e:
+        print('Error inserting new table:', e)
 
 # The string that identifies this type within the database.
-PAPER_DB_TYPE = 'paper'
+PAPER_DB_TYPE = 'papers'
 
 class Paper:
     """ Metadata for an academic paper I've read, including my notes.
 
     Also interfaces directly with database. """
+
+    columns = ['title', 'source', 'datePublished', 'dateRead', 'summary', 'futureWork', 'otherThoughts']
+
     def __init__(self, **kwargs):
         """ A constructor that automatically stores all keyword arguments.
 
         (Because I'm lazy.) """
-        self.__dict__.update(kwargs)
 
-        for param in ['title', 'source', 'datePublished', 'dateRead', 'comments']:
+        print('kwargs:', kwargs)
+        # Validate entry.
+        for param in Paper.columns:
             if param not in kwargs:
                 raise ValueError('Cannot instantiate paper without {}.'.format(param))
+        # Store.
+        self.__dict__.update(kwargs)
 
     def save(self):
-        """ Saves an object to the database. """
-        db.insert(self.to_json())
+        """ Save an object to the database and get its ID. """
+        db = get_db()
+        col_sql = '({})'.format(','.join(Paper.columns)) # '(title,source,...,otherThoughts)'
+        col_sql_vals = tuple(self.__dict__[x] for x in Paper.columns)
+        col_sql_q = '({})'.format(','.join(['?' for _ in Paper.columns]))
+        sql = ''' INSERT INTO {}{} VALUES{} '''.format(PAPER_DB_TYPE, col_sql, col_sql_q)
+        print('sql:', sql)
+        print('col_sql_vals:', col_sql_vals)
+        cur = db.cursor()
+        cur.execute(sql, col_sql_vals)
+        db.commit()
+        self.id = cur.lastrowid
+        print('set id:', self.id)
 
     def to_json(self):
         self_as_dict = vars(self)
@@ -32,8 +79,45 @@ class Paper:
         return self_as_dict
 
     @staticmethod
-    def get_all():
-        return db.search(where('type') == PAPER_DB_TYPE)
+    def from_sql_row(row):
+        obj_dict = dict(zip(['id'] + Paper.columns, row))
+        return Paper(**obj_dict)
+
+    @staticmethod
+    def get_paper_by_id(paper_id):
+        # Get papers from database 'papers' table by ID.
+        db = get_db()
+        db.row_factory = sqlite3.Row # This enables column access by name: row['column_name']
+        conn = db.cursor()
+        rows = conn.execute('SELECT * from {} WHERE id={}'.format(
+            PAPER_DB_TYPE, paper_id)
+        ).fetchall()
+        conn.close()
+        #@TODO: Throw error for multiple rows, and for 0 rows?
+        return Paper.from_sql_row(rows[0])
+
+    @staticmethod
+    def get_all(retry=False):
+        try:
+            # Get papers from database 'papers' table.
+            db = get_db()
+            db.row_factory = sqlite3.Row # This enables column access by name: row['column_name']
+
+            conn = db.cursor()
+            rows = conn.execute('SELECT * from {}'.format(PAPER_DB_TYPE)).fetchall()
+            conn.close()
+            # If no papers, return None instead of '[]'. :)
+            if rows == '[]':
+                return None
+            else:
+                return [Paper.from_sql_row(row) for row in rows]
+        except sqlite3.OperationalError as e:
+            if not retry:
+                # First error-- try inserting table.
+                create_paper_table()
+                return Paper.get_all(retry=True)
+            else:
+                raise e
 
     @classmethod
     def from_params(_class, params):
@@ -41,12 +125,15 @@ class Paper:
 
         For now, do not convert times to datetime objects; keep them as ISO
             strings. """
+        print('params:', params)
         return _class(
             title=params.get('title'),
             source=params.get('source'),
             datePublished=params.get('datePublished'),
             dateRead=params.get('dateRead'),
-            comments=params.get('comments'),
+            summary=params.get('summary'),
+            futureWork=params.get('futureWork'),
+            otherThoughts=params.get('otherThoughts'),
         )
 
     @classmethod
